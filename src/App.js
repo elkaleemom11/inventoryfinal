cc1bc026795c0ebcf69d7402edfecb67d67825da
Connie <elkaleemom11@gmail.com>
2018-09-22 18:36:50 -0600

    login page

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import SignIn from './Components/SignIn';
+import { Admin } from 'react-admin';
+import authProvider from './Components/authProvider';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      signin: []
+    }
+  }
+
+  getSignIn() {
+    this.setState({
+      signin: [
+        {
+        }
+      ]
+
+    });
+
+  }
+
+
+  componentDidMount() {
+    this.getSignIn();
+  }
+
+  handleSignIn(form) {
+    let SignIn = this.state.signin;
+    SignIn.push(form);
+    this.setState({ signin: SignIn });
+  }
+  render() {
+    return (
+      <Admin authProvider={authProvider}>
+        <SignIn signin={this.state.signin} />
+      </Admin>
+    );
+  }
+}
+
+
+
+export default App;
